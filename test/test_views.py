c35944d708e83dca3269879e3f1df6b78d017716
LukaNart <lukasz.ja2@gmail.com>
2018-02-17 14:57:43 +0100

    zmiana imienia sprawdzenie heroku

--- test/test_views.py
+++ test/test_views.py
@@ -15,5 +15,5 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEquals(
-            '{ "imie":"Lukasz","nazwisko":"Nartowski","mgs":"Hello World!"}',
+            '{ "imie":"Marcin","nazwisko":"Nartowski","mgs":"Hello World!"}',
             rv.data)
